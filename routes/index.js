23fa9e6a8e811fb19ec504675f78627335290b66
Malik Moeez Nawaz <malikmoeez.nawaz@studentambassadors.com>
2024-08-11 22:10:56 +0500

    card page is developed and products are showing in cart

--- routes/index.js
+++ routes/index.js
@@ -2,16 +2,33 @@ const express = require('express');
 const router = express.Router();
 const {isLoggedIn} = require('../middlewares/isLoggedIn');
 const productModel = require('../models/product-model');
+const userModel = require('../models/user-model');
+const { disconnect } = require('mongoose');
 
 router.get('/', (req, res) => {
     let error = req.flash("error");
-    res.render('index', {error});
+    res.render('index', {error, loggedin: false});
 });
-
 
 router.get("/shop", isLoggedIn, async (req, res) => {
     const products = await productModel.find();
-    res.render("shop", {products});
+    let success = req.flash("success");
+    res.render("shop", {products, success});
+});
+
+router.get("/cart", isLoggedIn, async (req, res) => {
+    let user = await userModel.findOne({email: req.user.email}).populate("cart");
+
+    const bill = (Number(user.cart[0].price)) + (Number(user.cart[0].discount)) + 20;
+    res.render("cart", {user, bill});
+}); 
+
+router.get("/addtocart/:productid", isLoggedIn, async (req, res) => {
+    let user = await userModel.findOne({email: req.user.email})
+    user.cart.push(req.params.productid);
+    await user.save();
+    req.flash("success", "Product added to cart");
+    res.redirect("/shop");
 });
 
 router.get("/logout", isLoggedIn, (req, res) => {
